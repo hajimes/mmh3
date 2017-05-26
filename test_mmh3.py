cef331049b5360c0997ceb376005a0ec504797ee
Hajime Senuma <hajime.senuma@gmail.com>
2017-05-26 18:00:54 +0900

    Add AppVeyor support

--- test_mmh3.py
+++ test_mmh3.py
@@ -1,6 +1,14 @@
 import mmh3
 
+
 # Note that MurmurHash3 is endian-sensitive.
 # In big-endian environments, these tests may fail.
 def test_hash_value():
     assert mmh3.hash('foo') == -156908512
+    
+    # Several test vectors devised by Ian Boyd
+    # https://stackoverflow.com/a/31929528
+    assert mmh3.hash('', seed=0) == 0
+    assert mmh3.hash('', seed=1) == 0x514E28B7
+    assert mmh3.hash('\0\0\0\0', seed=0) == 0x2362F9DE
+    # To be added
