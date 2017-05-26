aa676101e1fa73f6a82ea954c92584fe72815519
Hajime Senuma <hajime.senuma@gmail.com>
2017-05-27 02:08:51 +0900

    wintest

--- mmh3module.cpp
+++ mmh3module.cpp
@@ -10,14 +10,12 @@
 #include "MurmurHash3.h"
 
 #if defined(_MSC_VER)
-#ifndef _MSC_STDINT_H_
-typedef signed char int8_t;
+typedef signed __int8 int8_t;
 typedef signed __int32 int32_t;
 typedef signed __int64 int64_t;
-typedef unsigned char uint8_t;
-typedef unsigned long uint32_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int32 uint32_t;
 typedef unsigned __int64 uint64_t;
-#endif // ifndef _MSC_STDINT_H_
 // Other compilers
 #else    // defined(_MSC_VER)
 #include <stdint.h>
