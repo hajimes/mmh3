d81c67c6dc9e9fd47924477df726f050169efae4
Hajime Senuma <hajime.senuma@gmail.com>
2024-09-07 00:45:31 +0900

    Use function inlining

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import Extension, setup
 
 mmh3module = Extension(
-    "mmh3", sources=["./src/mmh3/mmh3module.c", "./src/mmh3/murmurhash3.c"]
+    "mmh3", sources=["./src/mmh3/mmh3module.c"]
 )
 
 setup(
