5257444228b8604aa44cb0b0bc475ef08d65c5e9
Hajime Senuma <hajime.senuma@gmail.com>
2024-09-07 00:48:41 +0900

    Fix lint errors

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@
 
 from setuptools import Extension, setup
 
-mmh3module = Extension(
-    "mmh3", sources=["./src/mmh3/mmh3module.c"]
-)
+mmh3module = Extension("mmh3", sources=["./src/mmh3/mmh3module.c"])
 
 setup(
     ext_modules=[mmh3module],
