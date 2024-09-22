70ef57d342244d7bc9c90cfd9968cb46591ca96c
Hajime Senuma <hajime.senuma@gmail.com>
2024-09-22 23:37:09 +0900

    Remove stub file

--- foo.sh
+++
@@ -1,5 +0,0 @@
-          if [[ "aas" == "windows-2022" ]]; then
-            for %i in (dist\*.tar.gz) do python -m pip install %i
-          else
-            python -m pip install dist/*.tar.gz
-          fi
